1187ac8e71864264ed0123c7e66ea02713dbf11b
Luis Gabriel <janco7249@gmail.com>
2022-04-29 17:26:06 -0400

    feat(Root): Adding new types for Schemas of collections

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,6 +8,35 @@ export interface DiaryEntry {
   comment: string
 }
 
+export interface User {
+  username: string
+  email: string
+  photoUrl: string
+  about: string
+  servers: string[]
+  channels: string[]
+}
+
+export interface Server {
+  serverName: string
+  users: string[]
+  rols: string[]
+  channels: string[]
+}
+
+export interface Channel {
+  serverId: string
+  channelName: string
+  section: string
+  messages: string[]
+  owner: string[]
+}
+
+export interface Message {
+  message: string
+  author: string
+}
+
 // export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
 export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
 export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
