c8254a4a93ba5d19aefd313e11ab87d5135a3ef1
Luis Gabriel <janco7249@gmail.com>
2022-05-02 23:24:27 -0400

    feat(Root): Add new routes for channels

--- src/routes/channel.routes.ts
+++ src/routes/channel.routes.ts
@@ -3,6 +3,14 @@ import * as channelCtrl from '../controllers/channel.controll'
 
 const router = Router()
 
+router.get('/', channelCtrl.getAllChannel)
+
+router.get('/msg', channelCtrl.getAllMessages)
+
+router.get('/:id', channelCtrl.getChannel)
+
 router.post('/:myId/:yourId', channelCtrl.createChannel)
+
+router.delete('/:userId/:channelId', channelCtrl.deleteChannel)
 
 export default router
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -9,6 +9,4 @@ router.get('/:username', userCtrl.getUser)
 
 router.get('/', userCtrl.getAllUsers)
 
-router.delete('/channel/:userId/:channelId', userCtrl.deleteChannel)
-
 export default router
