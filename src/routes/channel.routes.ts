baadd076d95b64c97075e7483b8f1ee2ab2799d8
Luis Gabriel <janco7249@gmail.com>
2022-05-04 21:51:26 -0400

    feat(Root): Add routes to messages

--- src/routes/channel.routes.ts
+++ src/routes/channel.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 import * as channelCtrl from '../controllers/channel.controll'
+import * as messageCtrl from '../controllers/message.controll'
 
 const router = Router()
+// Messages
+router.get('/msg', messageCtrl.getAllMessages)
 
+router.post('/msg', messageCtrl.createMessage)
+
+router.delete('/msg/:msgId/:channelId', messageCtrl.deleteMessage)
+
+// Channel
 router.get('/', channelCtrl.getAllChannel)
-
-router.get('/msg', channelCtrl.getAllMessages)
 
 router.get('/:id', channelCtrl.getChannel)
 
