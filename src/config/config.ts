c976509a131e4f1fd0a41b3d73ac232cc4251e09
Luis Gabriel <janco7249@gmail.com>
2022-05-12 16:46:14 -0400

    fix(Root): Add environment variable conditionally

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  PORT: process.env.PORT ?? 3000,
-  MONGO_URI: process.env.URI_CLUSTER_DB ?? '',
-  NODE_ENV: process.env.NODE_ENV ?? ''
+  PORT: process.env.PORT != null ? process.env.PORT : 3000,
+  MONGO_URI: process.env.URI_CLUSTER_DB != null ? process.env.URI_CLUSTER_DB : '',
+  NODE_ENV: process.env.NODE_ENV != null ? process.env.NODE_ENV : ''
 }
