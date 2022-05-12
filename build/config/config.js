5db8bdac0842809d76cf9623be03529f2f2df47e
Luis Gabriel <janco7249@gmail.com>
2022-05-12 16:50:36 -0400

    fix(Root): Update build/config.js of envintonments

--- build/config/config.js
+++ build/config/config.js
@@ -2,12 +2,11 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a, _b, _c;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.default = {
-    PORT: (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000,
-    MONGO_URI: (_b = process.env.URI_CLUSTER_DB) !== null && _b !== void 0 ? _b : '',
-    NODE_ENV: (_c = process.env.NODE_ENV) !== null && _c !== void 0 ? _c : ''
+    PORT: process.env.PORT != null ? process.env.PORT : 3000,
+    MONGO_URI: process.env.URI_CLUSTER_DB != null ? process.env.URI_CLUSTER_DB : '',
+    NODE_ENV: process.env.NODE_ENV != null ? process.env.NODE_ENV : ''
 };
