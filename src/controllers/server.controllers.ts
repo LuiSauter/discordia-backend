d28b6d1d3971696e7f0a3ac3ab640461d8a0f95a
Luis Gabriel <janco7249@gmail.com>
2022-05-21 14:40:10 -0400

    feat(Root): Added crud for server and routes for server feat(Root): Added crud and routes for server

--- src/controllers/server.controllers.ts
+++ src/controllers/server.controllers.ts
@@ -1,31 +1,83 @@
 import { RequestHandler } from 'express'
+import ServerModel from '../models/server'
+import ChannelModel from '../models/channel'
+import UserModel from '../models/user'
 
 export const allServers: RequestHandler = async (_req, res) => {
   try {
-    res.sendStatus(204)
+    const servers = await ServerModel.find({})
+    res.status(200).json(servers)
   } catch (error) {
     res.sendStatus(406)
   }
 }
 
-export const getServer: RequestHandler = async (_req, res) => {
+export const getServer: RequestHandler = async (req, res) => {
   try {
-    res.sendStatus(204)
+    const server = await ServerModel.findById(req.params.id).populate('channels')
+    res.status(200).json(server)
   } catch (error) {
     res.sendStatus(406)
   }
 }
 
-export const createServer: RequestHandler = async (_req, res) => {
+export const createServer: RequestHandler = async (req, res) => {
   try {
+    console.log(req.body)
+    const newServer = await ServerModel.create({
+      serverName: req.body.serverName,
+      users: [req.body.user],
+      admin: req.body.user,
+      channels: []
+    })
+    await newServer.save()
+    const defaultChannelOne = await ChannelModel.create({
+      channelName: 'bienvenida-y-reglas',
+      serverId: newServer._id,
+      section: 'información',
+      messages: [],
+      owner: [req.body.user]
+    })
+    await defaultChannelOne.save()
+    const defaultChannelTwo = await ChannelModel.create({
+      channelName: 'general',
+      serverId: newServer._id,
+      section: 'canales de texto',
+      messages: [],
+      owner: [req.body.user]
+    })
+    await defaultChannelTwo.save()
+
+    const defaultChannelThree = await ChannelModel.create({
+      channelName: 'Sala de estudio',
+      serverId: newServer._id,
+      section: 'canales de voz',
+      messages: [],
+      owner: [req.body.user]
+    })
+    await defaultChannelThree.save()
+    const update = {
+      $push: {
+        channels: [defaultChannelOne._id, defaultChannelTwo._id, defaultChannelThree._id]
+      }
+    }
+    await UserModel.findByIdAndUpdate(req.body.user, {
+      $push: { servers: [newServer._id] }
+    })
+    await ServerModel.findByIdAndUpdate(newServer._id, update)
+
     res.sendStatus(201)
   } catch (error) {
+    console.log(error)
     res.sendStatus(406)
   }
 }
 
-export const deleteServer: RequestHandler = async (_req, res) => {
+export const deleteServer: RequestHandler = async (req, res) => {
   try {
+    const findServer = await ServerModel.findById(req.params.serverId)
+    await ChannelModel.deleteMany({ _id: { $in: findServer?.channels } })
+    await ServerModel.findByIdAndDelete(req.params.serverId)
     res.sendStatus(200)
   } catch (error) {
     res.sendStatus(406)
--- src/routes/server.routes.ts
+++ src/routes/server.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
-// import * as serverCtrl from '../controllers/server.controllers'
+import * as serverCtrl from '../controllers/server.controllers'
 const router = Router()
 
-// router.post('/', serverCtrl.createServer)
+router.post('/add', serverCtrl.createServer)
+
+router.get('/', serverCtrl.allServers)
+
+router.get('/:id', serverCtrl.getServer)
+
+router.delete('/:myId/:serverId', serverCtrl.deleteServer)
 
 export default router
